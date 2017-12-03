74899aa90a71070c63cfd1863e940726a2991e53
Mentesnot Aboset <mentesnot.aboset@gmail.com>
2017-12-02 20:22:50 -0500

    initial commit

--- app/src/main/java/com/techprax/comp3074_project13/LaunchActivity.java
+++ app/src/main/java/com/techprax/comp3074_project13/LaunchActivity.java
@@ -1,18 +1,26 @@
 package com.techprax.comp3074_project13;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class LaunchActivity extends AppCompatActivity {
 
-    private static int TIME_OUT = 4000;
+    private static int TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_launch);
 
         final View myLayout = findViewById(R.id.launch);
@@ -24,5 +32,6 @@ public class LaunchActivity extends AppCompatActivity {
                 finish();
             }
         }, TIME_OUT);
+
     }
 }
